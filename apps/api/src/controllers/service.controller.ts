c1eeda10cc3c16ee13637776c2a9032a1976c592
Oluwadare Daniel <oluwadare458@gmail.com>
2026-07-16 18:09:26 +0100

    feat(marketplace): initialize ServiceController

---
+++ apps/api/src/controllers/service.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+import { ServiceService } from '../services/service.service';
+import { sendSuccess, sendError } from '../utils/response';
+
+export class ServiceController {
+  // Methods will be added here
+}
