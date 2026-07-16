3f7794cb5a6aa9104dc28970f79251b7a74b1d76
Oluwadare Daniel <oluwadare458@gmail.com>
2026-07-16 18:06:21 +0100

    feat(registry): implement service creation logic

--- apps/api/src/services/service.service.ts
+++ apps/api/src/services/service.service.ts
@@ -1,5 +1,28 @@
 import prisma from 'database';
 
 export class ServiceService {
-  // Structure for Service logic
+  static async createService(data: {
+    providerId: string;
+    name: string;
+    description: string;
+    categoryName: string;
+    endpoint: string;
+    price: number;
+    paymentRequired?: boolean;
+  }) {
+    return await prisma.service.create({
+      data: {
+        providerId: data.providerId,
+        name: data.name,
+        description: data.description,
+        categoryName: data.categoryName,
+        endpoint: data.endpoint,
+        price: data.price,
+        paymentRequired: data.paymentRequired ?? true,
+        analytics: {
+          create: {}
+        }
+      }
+    });
+  }
 }
